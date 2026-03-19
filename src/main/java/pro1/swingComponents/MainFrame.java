452d6145661df3f366f701705cb0d7658770aa54
Filip <78358886+Fylyb@users.noreply.github.com>
2026-03-19 22:21:53 +0100

    Refactor MainFrame to improve drawing functionality

--- src/main/java/pro1/swingComponents/MainFrame.java
+++ src/main/java/pro1/swingComponents/MainFrame.java
@@ -10,10 +10,12 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 public class MainFrame extends JFrame {
-    private DisplayPanel displayPanel;
+    DisplayPanel displayPanel;
     private int x;
     private int y;
-    private String color = "#000000";
+    private String color;
+
+    private PolyLine polyLine = new PolyLine();
 
     public void setColor(String color) {
         this.color = color;
@@ -27,29 +29,33 @@ public class MainFrame extends JFrame {
         this.setExtendedState(JFrame.MAXIMIZED_BOTH);
 
         this.displayPanel = new DisplayPanel();
+        this.displayPanel.setDrawable(this.polyLine);
         this.add(this.displayPanel, BorderLayout.CENTER);
 
-        JPanel leftPanel = new OptionsPanel(this);
+        JPanel leftPanel = new OptionsPanel(this, this.polyLine, this.displayPanel);
+
         this.add(leftPanel, BorderLayout.WEST);
 
         this.displayPanel.addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
-                MainFrame.this.x = e.getX();
-                MainFrame.this.y = e.getY();
-                MainFrame.this.showExample();
+                polyLine.addPoint(e.getX(), e.getY());
+                displayPanel.setDrawable(polyLine);
+                displayPanel.repaint();
             }
         });
+        this.setVisible(true);
     }
 
-    public void showExample(){
-        MainFrame.this.displayPanel.setDrawable(this.example());
+    public void showExample() {
+        this.displayPanel.setDrawable(example());
     }
 
     private Drawable example() {
-        var d1 = new Ellipse(0, 0, 150, 250, this.color);
-        var d2 = new Text(0, 0, this.color);
-        var d3 = new Line(0, 50,170,170,3, this.color);
+//        var color = ColorUtils.randomColor();
+        var d1 = new Ellipse(0, 0, 150, 250, color);
+        var d2 = new Text(0, 0, color);
+        var d3 = new Line(0, 50, 170, 170, 3, color);
         return new Group(new Drawable[]{d1, d2, d3}, this.x, this.y, 40, 1, 1);
     }
 }
